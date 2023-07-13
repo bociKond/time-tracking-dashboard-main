1b4240a88051383166f987813dbbfc5eb5433472
Kaloyan Todorov <107853936+bociKond@users.noreply.github.com>
2023-07-13 10:16:20 +0300

    Update app.js
    Removed unnecessary stuff

--- app.js
+++ app.js
@@ -1,44 +1,12 @@
-// const dailyLink = document.getElementsByClassName("daily");
-// const weeklyLink = document.getElementsByClassName("weekly");
-// const monthlyLink = document.getElementsByClassName("monthly");
-
-// const dailyInfo = document.getElementById("daily");
-// const weeklyInfo = document.getElementById("weekly");
-// const monthlyInfo = document.getElementById("monthly");
-
-// dailyLink.onclick = function () {
-//   dailyInfo.style.display = "flex";
-//   weeklyInfo.style.display = "none";
-//   monthlyInfo.style.display = "none";
-// };
-
-// weeklyLink.onclick = function () {
-//   dailyInfo.style.display = "none";
-//   weeklyInfo.style.display = "flex";
-//   monthlyInfo.style.display = "none";
-// };
-
-// monthlyLink.onclick = function () {
-//   dailyInfo.style.display = "none";
-//   weeklyInfo.style.display = "none";
-//   monthlyInfo.style.display = "flex";
-// };
-
-// Get reference to the navigation bar container
 const navContainer = document.querySelector(".sidebar-navigation");
-
-// Get references to the dashboard information elements
 const infoElements = document.querySelectorAll(".info");
 
-// Add click event listener to the navigation bar container
 navContainer.addEventListener("click", (event) => {
   event.preventDefault();
 
-  // Check if a navigation link was clicked
   if (event.target.classList.contains("nav-link")) {
     const targetId = event.target.getAttribute("data-target");
 
-    // Toggle the visibility of the corresponding information element
     infoElements.forEach((info) => {
       if (info.id === targetId) {
         info.style.display = "flex";
